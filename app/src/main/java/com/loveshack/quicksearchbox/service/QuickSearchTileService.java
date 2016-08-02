9572bfa5d5e4009f566502a5dbf04394b50e44ff
Joseph Antonetti <antonet2@illinois.edu>
2016-08-02 18:26:38 -0500

    Added in search box

--- app/src/main/java/com/loveshack/quicksearchbox/service/QuickSearchTileService.java
+++ app/src/main/java/com/loveshack/quicksearchbox/service/QuickSearchTileService.java
@@ -1,5 +1,6 @@
 package com.loveshack.quicksearchbox.service;
 
+import android.content.Intent;
 import android.service.quicksettings.TileService;
 
 /**
@@ -8,4 +9,11 @@ import android.service.quicksettings.TileService;
  * Created by joe on 8/2/16.
  */
 public class QuickSearchTileService extends TileService {
+
+    @Override
+    public void onClick(){
+        super.onClick();
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        startActivityAndCollapse(intent);
+    }
 }
